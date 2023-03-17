9c87d115e875d9718ac44bd3061349c8bd5d5762
allaouinassim <allaouinassim15@gmail.com>
2023-03-16 18:57:29 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*return: Always 0 (Success)
+*main - main block
+*return: 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
