6c45456f3eb92e06d6087f7703c32063b80e03ec
allaouinassim <allaouinassim15@gmail.com>
2023-05-02 08:48:01 -0700

    T0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * h: linked list of type listint_t to print
+ * Return: (Success)
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t Num = 0;
+
+	while (h)
+	{
+	printf("%d\n", h->n);
+	Num++;
+	h = h->next;
+	}
+	return (Num);
+}
